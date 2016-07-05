6967ce2f87349f358b20938dc2582a8cda3ddf30
dpfaffenbauer <dominik@lineofcode.at>
2016-07-05 17:08:09 +0200

    update header informations and add class headers

--- lib/CoreShop/Model/PriceRule/Action/ApiPrice.php
+++ lib/CoreShop/Model/PriceRule/Action/ApiPrice.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * CoreShop.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2016 Dominik Pfaffenbauer (http://www.pfaffenbauer.at)
+ * @license    http://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
 
 namespace CoreShop\Model\PriceRule\Action;
 
@@ -10,7 +22,8 @@ use CoreShop\Tool;
 use Pimcore\Cache;
 
 /**
- * Class DiscountAmount.
+ * Class ApiPrice
+ * @package CoreShop\Model\PriceRule\Action
  */
 class ApiPrice extends AbstractAction
 {
--- lib/CoreShopExamples/Plugin.php
+++ lib/CoreShopExamples/Plugin.php
@@ -1,10 +1,26 @@
 <?php
+/**
+ * CoreShop.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2016 Dominik Pfaffenbauer (http://www.pfaffenbauer.at)
+ * @license    http://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+ */
 
 namespace CoreShopExamples;
 
 use CoreShop\Model\Product\PriceRule;
 use Pimcore\API\Plugin as PluginLib;
 
+/**
+ * Class Plugin
+ * @package CoreShopExamples
+ */
 class Plugin extends PluginLib\AbstractPlugin implements PluginLib\PluginInterface
 {
     public function init()
